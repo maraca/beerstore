f3914a52183521ac57dfeb0b6af462218e1c7f05
Maraca <contact@martincozzi.com>
2011-09-20 22:42:44 -0700

    Models added.

---
+++ store/models.py
@@ -0,0 +1,101 @@
+from django.db import models
+
+BEVERAGE_TYPES = (
+    ('B', 'Beer'),
+    ('W', 'Wine'),
+    ('O', 'Other'),
+)
+    
+
+class Product(models.Model):
+    """Basic Product Definition."""
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True,
+                                   null=True)
+    slug = models.SlugField(max_length=120,
+                            db_index=True,
+                            unique=True)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
+    disabled = models.BooleanField()
+
+    class Meta:
+        abstract = True
+
+    def __unicode__(self):
+        return self.slug
+
+
+class Beverage(Product):
+    """Built to abstract Beer and Wine similarities."""
+    
+    SIZE_UNIT_TYPES = (
+        ('OZ', 'OZ'),
+    )
+
+    CONTAINER_TYPES = (
+        ('B', 'BTL'),
+        ('C', 'CAN'),
+    )
+
+    alcohol = models.DecimalField(max_digits=5,
+                                  decimal_places=2)
+    company = models.ForeignKey('Company')
+    # country : TODO
+    size_unit = models.CharField(max_length=3, 
+                                 choices=SIZE_UNIT_TYPES) # OZ , mm, Pack
+    size_value = models.CharField(max_length=10) # 22, 4, 6, 750
+    container = models.CharField(max_length=2,
+                                 choices=CONTAINER_TYPES) # BTL, CAN, Box etc. 
+    abv = models.DecimalField(max_digits=4,
+                              decimal_places=2)
+    
+
+class BeverageStyle(models.Model):
+    """Beverage style.
+
+    eg: Ale / IPA / Lager / Cabernet etc. 
+    """
+    name = models.CharField(max_length=150)
+    beverage_type = models.CharField(max_length=2,
+                                     choices=BEVERAGE_TYPES)
+
+
+class WineStyle(BeverageStyle):
+
+    class Meta:
+        proxy = True
+
+
+class BeerStyle(BeverageStyle):
+
+    class Meta:
+        proxy = True
+
+
+class Beer(Product):
+    """Beer Definition."""
+    ibu = models.IntegerField()
+    """
+    Bitterness shown in International Bittering Units, where:
+        - Low bitterness 0 - 20
+        - Moderate bitterness 21 - 30
+        - Noticeable bitterness 31 and over
+    """
+    style = models.ForeignKey('BeerStyle')# Ale / IPA / Lager / Cabernet 
+
+
+class Wine(Product):
+    """Wine Definition."""
+    # region = 
+    # vintage =
+    style = models.ForeignKey('WineStyle') # 
+
+
+class Company(models.Model):
+    """Company name for Beverage."""
+
+    beverage_type = models.CharField(max_length=2,
+                                     choices=BEVERAGE_TYPES)
+    name = models.CharField(max_length=255)
--- store/settings.py
+++ store/settings.py
@@ -14,8 +14,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'beerdb',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -123,6 +123,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'store',
 )
 
 # A sample logging configuration. The only tangible logging
